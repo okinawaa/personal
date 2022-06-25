13ba3730ad0ae741367f69cf6a79a6cba08d097a
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-06-25 19:50:00 +0900

    feat: styling & commong things setting

---
+++ src/@types/global.d.ts
@@ -0,0 +1,2 @@
+type LangType = "ko-KR" | "en-US" | "ja-JP";
+type AnimationDirectionType = "up" | "down" | "left" | "right" | "stop";
---
+++ src/constants/Variables.ts
@@ -0,0 +1,16 @@
+// 데스크톱
+export const XD_DESKTOP_WIDTH = 1920;
+export const XD_DESKTOP_HEIGHT = 1080;
+
+// 노트북
+export const XD_LAPTOP_WIDTH = 1024;
+
+// 태블릿
+export const XD_TABLET_WIDTH = 768;
+
+// 모바일
+export const XD_MOBILE_WIDTH = 384;
+
+export const BOLD_WEIGHT = 700;
+
+export const ROOT_FONT_SIZE = 10;
