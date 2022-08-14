53fa66675e5e7e0bf08920650877f15733e4c57f
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-08-15 01:38:47 +0900

    feat: mobile 반응형 지원

--- src/pages/Main/Main.style.ts
+++ src/pages/Main/Main.style.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import Colors from "~/constants/Colors";
 import { FlexBox } from "~/constants/Common.style";
+import { media } from "~/constants/Media";
 import { pxToRem } from "~/constants/Size";
 
 export const Container = styled(FlexBox)`
   width: ${pxToRem(850)};
   padding: ${pxToRem(150)} 0;
+
+  ${media.tablet} {
+    width: ${pxToRem(350)};
+    padding: ${pxToRem(50)} ${pxToRem(10)};
+  }
 `;
 
 export const ItemWrapper = styled(FlexBox)`
