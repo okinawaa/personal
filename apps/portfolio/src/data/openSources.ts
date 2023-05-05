981304178be4746d1af5edbe4e8ef9e26847006e
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-05-05 14:39:13 +0900

    오픈소스 업데이트

--- apps/portfolio/src/data/openSources.ts
+++ apps/portfolio/src/data/openSources.ts
@@ -4,6 +4,14 @@ const openSources = [
     link: "https://github.com/toss/slash/issues?q=involves%3AChanhyukPark-Tech"
   },
   {
+    title: "suspensive | Open source for React suspense",
+    link: "https://github.com/suspensive/react/issues?q=involves%3AChanhyukPark-Tech"
+  },
+  {
+    title: "ark ui | Universal headless UI components for the web",
+    link: "https://github.com/chakra-ui/ark/issues?q=involves%3AChanhyukPark-Tech"
+  },
+  {
     title: "falso | All the Fake Data for All Your Real Needs",
     link: "https://github.com/ngneat/falso/issues?q=involves%3AChanhyukPark-Tech"
   },
