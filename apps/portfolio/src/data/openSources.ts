87cee148e5f5ad3cb90935845e9c030181aa96a9
박찬혁 <pgg6713@gmail.com>
2023-07-28 14:26:03 +0900

    Update openSources.ts

--- apps/portfolio/src/data/openSources.ts
+++ apps/portfolio/src/data/openSources.ts
@@ -1,35 +1,23 @@
 const openSources = [
   {
     title: "toss/slash",
-    link: "https://github.com/toss/slash/issues?q=involves%3AChanhyukPark-Tech"
+    link: "https://github.com/toss/slash/issues?q=involves%3Aokinawaa"
   },
   {
     title: "suspensive | Open source for React suspense",
-    link: "https://github.com/suspensive/react/issues?q=involves%3AChanhyukPark-Tech"
+    link: "https://github.com/suspensive/react/issues?q=involves%3Aokinawaa"
   },
   {
     title: "ark ui | Universal headless UI components for the web",
-    link: "https://github.com/chakra-ui/ark/issues?q=involves%3AChanhyukPark-Tech"
+    link: "https://github.com/chakra-ui/ark/issues?q=involves%3Aokinawaa"
   },
   {
     title: "falso | All the Fake Data for All Your Real Needs",
-    link: "https://github.com/ngneat/falso/issues?q=involves%3AChanhyukPark-Tech"
-  },
-  {
-    title: "nextauthjs/next-auth",
-    link: "https://github.com/nextauthjs/next-auth/issues?q=involves%3AChanhyukPark-Tech"
-  },
-  {
-    title: "carbon/carbon-design-system",
-    link: "https://github.com/carbon-design-system/carbon/pull/11577#event-6779481229"
+    link: "https://github.com/ngneat/falso/issues?q=involves%3Aokinawaa"
   },
   {
     title: "pine-lab/default-hooks",
     link: "https://github.com/bigpicture-kr/default-hooks"
-  },
-  {
-    title: "react-query-tutorial",
-    link: "https://github.com/ssi02014/react-query-tutorial"
   },
   {
     title: "flutter_getx_boilerplate",
